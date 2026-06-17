4a4ae8a461ca1a1a63fa8ed51f94f72eacedd6b4
Moises <moises842248@gmail.com>
2026-06-17 11:00:59 +0000

    trabajo terminado

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
 //write your code here
-console.log("Hello Rigo from the console!");
+const juego = (ourchoice) => {
 
+    const userchoice = ourchoice
+
+    const pcchoice = ["rock", "paper", "scissors", "lizard", "spock"];
+
+    const choicesRandom = pcchoice[Math.floor(Math.random() * pcchoice.length)];
+
+    console.log("Your choice"+ " " + ourchoice, "Pc choice" + " " + choicesRandom);
+
+    const rules = {
+        rock: ["scissors", "lizard"],
+        paper: ["rock", "spock"],
+        scissors: ["paper", "lizard"],
+        lizard: ["paper", "spock"],
+        spock: ["scissors", "rock"]
+    };
+
+    if (userchoice === choicesRandom) {
+        console.log("draw")
+
+    } else if (rules[userchoice].includes(choicesRandom)) {
+
+        console.log("You won")
+
+    } else {
+        console.log("You lost")
+    }
+
+    return "Your choice"+ " " + ourchoice, "Pc choice" + " " + choicesRandom
+
+};
+
+console.log(juego("paper"))
